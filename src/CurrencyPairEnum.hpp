bfe495a78de7a44bd6b3b1d0b710ea91ff961d5e
george <ruan3@illinois.edu>
2017-08-17 00:38:11 -0500

    Add preliminary exchange reader BFX

---
+++ src/CurrencyPairEnum.hpp
@@ -0,0 +1,13 @@
+/**
+ * CurrencyPairEnum is responsible for holding all pairs of currencies
+ */
+
+#pragma once
+
+enum CurrencyPairEnum {
+  BTCUSD,
+  ETHBTC,
+  ETHUSD,
+  LTCBTC,
+  LTCUSD,
+};
--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,29 @@
 #include <iostream>
 
 #include "ConfigStore.hpp"
+#include "ExchangeReaderBFX.hpp"
 
 const std::string ALPHA_VANTAGE_API_KEY = "alpha_vantage_api_key";
 
 int main() {
+  ExchangeReaderBFX reader;
+  reader.subscribeToOrderbookChannel(CurrencyPairEnum::BTCUSD, "P2", "F1");
+
   std::ifstream input("config.txt");
   ConfigStore& config = ConfigStore::get();
   config.parseFile(input);
-  auto maybeVal = config.getMaybeValue(std::string(ALPHA_VANTAGE_API_KEY));
+  auto maybeVal = config.getMaybeValue(ALPHA_VANTAGE_API_KEY);
 
-  if (maybeVal->empty()) {
+  if (!maybeVal) {
     std::cerr << "Config key: "
       << ALPHA_VANTAGE_API_KEY
-      << "not found."
+      << " not found."
       << std::endl;
     return 1;
   }
   std::cout << maybeVal.value()<< std::endl;
 
+
+  while (true) {};
   return 0;
 }
